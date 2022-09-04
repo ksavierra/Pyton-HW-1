edb2f5595f6467849a3319f101ef0ddd225e2851
Kseniya <ksavierra@rambler.ru>
2022-09-04 12:10:08 +0500

    мократила код в решении 1й задачи

--- lesson-1.py
+++ lesson-1.py
@@ -1,15 +1,18 @@
 # 1. Напишите программу, которая принимает на вход цифру, 
 # обозначающую день недели, и проверяет, является ли этот день выходным.
 
-print('Введите первое число: ')
-first = float(input())
-print('Введите второе число: ')
-second = float(input())
-if first == second*second:
-    print('Да, первоe число является квадратом второго числа')
-elif second == first*first:
-    print('Да, второе число является квадратом первого числа')
-else:
-    print('Нет, эти числа не являются квадратами друг друга')
+print('Введите день недели: ')
+x = float(input())
 
+def f(x):
+     if x < 1 or x > 7:
+         return 'В неделе 7 дней'
+     elif x == 1 or x == 2 or x == 3 or x == 4 or x == 5:
+         return 'Будни'
+     elif x == 6 or x == 7:
+         return 'Выходной'
+     else:
+         return
+day = x        
+print(f(day))
 
